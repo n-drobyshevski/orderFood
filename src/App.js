909b62a9368e610d58cd57e047f6abd228cfdec3
Nick <n.drobyshevski@pm.me>
2022-08-15 16:57:07 +0200

    Add basic structure and ItemsList component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import ItemsList from './components/Items/ItemsList'
+
 function App() {
+  const testData = [
+    {id:'M1',name: 'Meal 1', description:'some desc', price:'$90.00'},
+    {id:'M2',name: 'Meal 2', description:'some desc', price:'$10.99'},
+    {id:'M3',name: 'Meal 3', description:'some desc', price:'$25.55'},
+    {id:'M4',name: 'Meal 4', description:'some desc', price:'$54.05'},
+  ]
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+  <React.Fragment>
+    <Header />
+    <ItemsList items={testData}/>
+  </React.Fragment>    
   );
 }
 
