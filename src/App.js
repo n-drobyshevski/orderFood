1faa339d6ab7499c5753ed67e44dd3e970371781
Nick <n.drobyshevski@pm.me>
2022-08-17 10:53:38 +0200

    Refactor adding to the cart functionality with useReducer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import Header from "./components/Header/Header";
 import About from "./components/About/About"
 import ItemsList from './components/Items/ItemsList'
 import CartModal from "./components/Cart/CartModal";
 
+
+const testData = [
+  { id: 'M1', name: 'Meal 1', description: 'some desc', price: '$90.00' },
+  { id: 'M2', name: 'Meal 2', description: 'some desc', price: '$10.99' },
+  { id: 'M3', name: 'Meal 3', description: 'some desc', price: '$25.55' },
+  { id: 'M4', name: 'Meal 4', description: 'some desc', price: '$54.05' },
+];
+
+const getItemById = (id) => {
+  for (let item of testData) {
+    if (item.id === id) {
+      return item
+    }
+  };
+};
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      const item = getItemById(action.itemId);
+      return [...state,  { ...item, ...{ amount: action.amount } }];
+    default:
+      return state;
+  }
+};
+
 function App() {
-  const testData = [
-    { id: 'M1', name: 'Meal 1', description: 'some desc', price: '$90.00' },
-    { id: 'M2', name: 'Meal 2', description: 'some desc', price: '$10.99' },
-    { id: 'M3', name: 'Meal 3', description: 'some desc', price: '$25.55' },
-    { id: 'M4', name: 'Meal 4', description: 'some desc', price: '$54.05' },
-  ];
 
   const [cartOpened, setCartOpened] = useState(false);
 
-  const [cartContent, setCartContent] = useState([]);
+  const [cartContent, dispatchCartContent] = useReducer(cartReducer, []);
 
   const closeCartHandler = () => {
     setCartOpened(false);
@@ -23,24 +43,41 @@ function App() {
   const openCartHandler = () => {
     setCartOpened(true);
   };
-  const addToCartHandler = (itemData) => {
 
-    let item = {};
-    for (const elem of testData) {
-      if (elem.id === itemData.id) {
-        item = elem;
-        break;
-      }
-    }
-    const newCartItem = { data: item, amount: itemData.amount };
+  const addToCartHandler = (itemToAddData) => {
 
-    setCartContent(prevState => {
-      return ([...prevState, newCartItem])
-    });
 
-    console.log('Item added to cart');
+    // for (let cartItem of cartContent) {
+    //   if (cartItem.id === itemToAddData.id) { // verifying if item is already in cart
+
+    //     setCartContent(prevState => {
+    //       prevState.map(item => {
+    //         if (cartItem.id === itemToAddData.id) {
+    //           item = { ...item, ...{ amount: item.amount + itemToAddData.amount } };
+    //         };
+    //       });
+    //       console.log(prevState)
+    //       return prevState;
+    //     });
+
+    //   }
+    //   return;
+    // }
+
+
+    // getting item data from testData 
+    dispatchCartContent({ type: 'ADD_TO_CART', itemId: itemToAddData.id, amount: itemToAddData.amount })
+    // setCartContent(prevState => {
+    //   return ([...prevState, newCartItem])
+    // });
 
   };
+
+  useEffect(() => {
+    console.log('CART UPDATED...');
+    console.table(cartContent);
+    console.log(cartContent);
+  }, [cartContent]);
 
   return (
     <React.Fragment>
--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -3,17 +3,16 @@ import './CartList.module.css';
 import CartItem from './CartItem';
 
 const CartList = (props) => {
-    console.log(props.items);
     return (
         <ul>
             {props.items.map((item) => {
                 // console.log(data);
                 return (
                     <CartItem
-                        key={item.data.id}
-                        name={item.data.name}
-                        desc={item.data.description}
-                        price={item.data.price}
+                        key={item.id}
+                        name={item.name}
+                        desc={item.description}
+                        price={item.price}
                         amount={item.amount}
                     />
                 );
