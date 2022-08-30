4458732ce1739a0987645185979ee15a422b2c99
Nick <n.drobyshevski@pm.me>
2022-08-30 13:52:19 +0200

    Add AddressModal form validation.

--- src/components/AddressModal/AddressModal.js
+++ src/components/AddressModal/AddressModal.js
@@ -5,11 +5,19 @@ import Button from '../UI/Button/Button';
 import Input from '../UI/Input/Input';
 
 import UserContext from '../../store/user-context';
-import { useRef, useContext } from 'react';
+import { useRef, useContext, useState } from 'react';
+
+const isEmpty = value => value.trim() === '';
+const isFiveChar = value => value.trim().length === 5;
 
 const AddressModal = props => {
     const userCtx = useContext(UserContext);
-    const userAddress = userCtx.address;
+    const [formInputsValidity, setFormInputsValidity] = useState({
+        houseNumber: true,
+        street: true,
+        city: true,
+        postalCode: true,
+    });
 
     const cityInputRef = useRef();
     const streetInputRef = useRef();
@@ -19,13 +27,41 @@ const AddressModal = props => {
 
     const formSubmitHandler = (event) => {
         event.preventDefault();
+
+        const postalCodeInputValue = postalCodeInputRef.current.value;
+        const cityInputValue = cityInputRef.current.value;
+        const streetInputValue = streetInputRef.current.value;
+        const houseNumberInputValue = numberInputRef.current.value;
+        const cityInputIsValid = !isEmpty(cityInputValue);
+        const streetInputIsValid = !isEmpty(streetInputValue);
+        const postalCodeInputIsValid = isFiveChar(postalCodeInputValue);
+        const houseNumberInputIsValid = !isEmpty(houseNumberInputValue);
+        console.log(postalCodeInputIsValid);
+
         console.log('form submit');
+
+        setFormInputsValidity({
+            houseNumber: houseNumberInputIsValid,
+            street: streetInputIsValid,
+            city: cityInputIsValid,
+            postalCode: postalCodeInputIsValid,
+        });
+
+        const formIsValid =
+            cityInputIsValid &&
+            streetInputIsValid &&
+            houseNumberInputIsValid &&
+            postalCodeInputIsValid;
+
+        if (!formIsValid) { return };
+
         const updateAddress = {
             city: cityInputRef.current.value,
             street: streetInputRef.current.value,
             postalCode: postalCodeInputRef.current.value,
             houseNumber: numberInputRef.current.value,
         };
+
         userCtx.updateUser('address', updateAddress);
     };
 
@@ -37,36 +73,44 @@ const AddressModal = props => {
             <main>
                 <form onSubmit={formSubmitHandler}>
                     <Input
+                        className={!formInputsValidity.city ? styles.invalid : ''}
                         ref={cityInputRef}
                         input={{
                             type: "text",
                             name: 'city',
                             placeholder: 'city',
                         }}
+                        error={!formInputsValidity.city && 'must not be empty'}
                         label={'City'} id={'city'} />
                     <Input
+                        className={!formInputsValidity.street ? styles.invalid : ''}
                         ref={streetInputRef}
                         input={{
                             type: "text",
                             name: 'street',
-                            placeholder: 'street'
+                            placeholder: 'street',
                         }}
+                        error={!formInputsValidity.street && 'must not be empty'}
                         label={'Street'} id={'street'} />
                     <Input
+                        className={!formInputsValidity.houseNumber ? styles.invalid : ''}
                         ref={numberInputRef}
                         input={{
                             type: "number",
                             name: 'number',
-                            placeholder: '01'
+                            placeholder: '00',
                         }}
-                        label={'building number'} id={'number'} />
+                        error={!formInputsValidity.houseNumber && 'must not be empty'}
+                        label={'house number'} id={'number'} />
                     <Input
+                        className={!formInputsValidity.postalCode ? styles.invalid : ''}
                         ref={postalCodeInputRef}
                         input={{
                             type: "number",
                             name: 'postal-code',
-                            placeholder: '00000'
+                            placeholder: '00000',
                         }}
+                        error={!formInputsValidity.postalCode && 'postal code must be 5 digit'}
                         label={'postal code'} id={'postal-code'} />
 
                     <div className={styles['form-actions']}>
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Input.module.css';
 
 const Input = React.forwardRef((props, ref) => {
-    const classes = `${styles.input} ${props.withIcon && styles['input-with-icon']}`
+    const classes = `${styles.input} ${props.withIcon ? styles['input-with-icon'] : ''} ${props.className}`
     return (
         <div className={classes}>
             {props.label && <label htmlFor={props.id}>{props.label}</label>}
@@ -12,6 +12,9 @@ const Input = React.forwardRef((props, ref) => {
                 {...props.input}
             >
             </input>
+            {props.error &&
+                <p>{props.error}</p>
+            }
         </div >
     );
 });
